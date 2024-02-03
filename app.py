b788e8170d18b2d8356e34a94d1b440c63c44999
Su Ruiyang <142382202+Su-Ruiyang@users.noreply.github.com>
2024-02-04 01:28:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, render_template
 import google.generativeai as palm
-palm.configure(api_key="")
-model = {"model":"models/chat-bison-001"}
+
+palm.configure(api_key="AIzaSyAZM8vfY-XfvxymR4PzHo35x8FTn0jP5RI")
+model = { 'model': "models/chat-bison-001"}
+
+
 app = Flask(__name__)
+
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
@@ -12,7 +16,8 @@ def index():
             **model,
             messages = q
         )
-        return(render_template("index.html",result=r.last))
+        return (render_template("index.html",result = r.last))
+        
     else:
         return(render_template("index.html", result="waiting for question............."))
 if __name__ == "__main__":
